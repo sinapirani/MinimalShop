ba40bd1ea96c83bdabdca5f4c29156dd0e3c5f6c
sinapirani <realsinapirani@gmail.com>
2022-01-17 10:49:34 +0330

    animation for slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container w-screen h-screen mx-auto flex justify-center items-center">
+      <div className="box w-2/4 h-2/4 bg-slate-800 rounded-lg ">
+        <img src="" alt="" />
+        <p className="name"></p>
+        <p className="des"></p>
+      </div>
     </div>
   );
 }
